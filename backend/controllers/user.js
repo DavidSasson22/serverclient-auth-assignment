2f83a61594249652d0d99fd9ee69220fbe3b4d47
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-08-04 22:24:19 +0300

    Create user.js

---
+++ backend/controllers/user.js
@@ -0,0 +1,102 @@
+const User = require("../models/user");
+
+//Register user
+const registerUser = async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
+
+//Login User
+const logUser = async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
+
+//Log out user
+const logOut = async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+};
+
+//Logout user from all devices
+const logOutAll = async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+};
+
+//Edit user's info
+const updateUser = async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = [
+    "userName",
+    "firstName",
+    "lastName",
+    "city",
+    "country",
+    "postalCode",
+    "isActive",
+    "email",
+    `password`,
+  ];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
+    res.send(req.user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
+
+//Delete user
+const deleteUser = async (req, res) => {
+  try {
+    await req.user.remove();
+    res.send(req.user);
+  } catch (e) {
+    res.status(500).send();
+  }
+};
+
+module.exports = {
+  registerUser,
+  logUser,
+  logOut,
+  logOutAll,
+  updateUser,
+  deleteUser,
+};
