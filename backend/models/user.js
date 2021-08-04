c900dafa77f8fbae6707ebd892a0889a4b0bb57e
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-08-04 22:17:37 +0300

    Create user.js
    user's model is ready

---
+++ backend/models/user.js
@@ -0,0 +1,149 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+const jwt = require(`jsonwebtoken`);
+
+const tokenJwt = process.env.tokenJwt;
+
+const userSchema = mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 20,
+    trim: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 20,
+    trim: true,
+  },
+
+  lastName: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 20,
+    trim: true,
+  },
+
+  city: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 20,
+    trim: true,
+  },
+
+  country: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 20,
+    trim: true,
+  },
+
+  postalCode: {
+    type: String,
+    required: true,
+    trim: true,
+    validate(value) {
+      if (!validator.isPostalCode(value)) {
+        throw new Error(`Postal code is invalid: ${value}`);
+      }
+    },
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error(`Email is invalid: ${value}`);
+      }
+    },
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error('Password cannot contain "password"');
+      }
+    },
+  },
+
+  joinedAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  isActive: {
+    type: Boolean,
+    required: false,
+    unique: false,
+    default: true,
+  },
+
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+//Don't send password and tokens array
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+  delete userObject.password;
+  delete userObject.tokens;
+  return userObject;
+};
+
+//generateAuthToken
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, tokenJwt);
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
+
+//Check if user's email matches user's password
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error(`Unable to login`);
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error(`Unable to login`);
+  }
+  return user;
+};
+
+//Hash password before saving
+userSchema.pre(`save`, async function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+const User = mongoose.model("Users", userSchema);
+
+module.exports = User;
