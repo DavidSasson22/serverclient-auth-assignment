5eb82b377dc3d468a947525d172f88a2b305f7ab
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-08-05 19:12:01 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -19,7 +19,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
-// import { atom, useRecoilState } from "recoil";
 import { RecoilRoot } from "recoil";
 
 // core components
