5f786cdb3e8d16d94a3b7cdc75d919e7d11585fc
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-08-08 14:56:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend/build")));
 
   // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+
+  app.get("/*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
   });
 }
 
