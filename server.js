5c1df7cd53598e9945f1d1453e4d6060f9928316
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-08-08 16:09:33 +0300

    new structure for deploy

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-require("./backend/dataBase/mongoose");
+require("./server/dataBase/mongoose");
 
-const userRouter = require("./backend/routs/user");
+const userRouter = require("./server/routs/user");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,12 +17,11 @@ app.use("/api/users", userRouter);
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "frontend/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   // Handle React routing, return all requests to React app
-
-  app.get("/*", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
